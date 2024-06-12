1c1f7c4bcf5ebd0038d1d642b073a1eedf3f511c
unknown <nguyen_van_phu@allgrow-labo.jp>
2024-06-12 16:57:05 +0700

    add fadeout

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,35 +1,40 @@
-let filterActive;
+/* ------------------------------ stockist page ----------------------------- */
 
-function filterCategory(category) {
-    if (filterActive != category) {
-        // reset results list
-        $('li.stockist_items').removeClass('active');
+if ($("#stockist")) {
+    let filterActive;
 
-        // elements to be filtered
-        $('li.stockist_items')
-            .filter('[data-tag="' + category + '"]')
-            .addClass('active');
+    function filterCategory(category) {
+        if (filterActive != category) {
+            // reset results list
+            $('li.stockist_items').removeClass('active');
 
-        // reset active filter
-        filterActive = category;
-        $('.js-stockist-option').removeClass('active');
+            // elements to be filtered
+            $('li.stockist_items')
+                .filter('[data-tag="' + category + '"]')
+                .addClass('active');
+
+            // reset active filter
+            filterActive = category;
+            $('.js-stockist-option').removeClass('active');
+        }
     }
+
+    $(window).on("pageshow load", function () {
+        const hash = window.location.hash.substring(1);
+
+        if (hash == "") {
+            $('.js-stockist-option').addClass('active');
+            $('li.stockist_items').addClass('active');
+        } else {
+            $('li.stockist_items')
+                .filter('[data-tag="' + hash + '"]')
+                .addClass('active');
+        }
+    });
+
+    $('.js-stockist-option').click(function () {
+        filterCategory($(this).attr('data-filter-tag'));
+        $(this).addClass('active');
+    });
 }
 
-$(window).on("pageshow load", function () {
-    const hash = window.location.hash.substring(1);
-
-    if (hash == "") {
-        $('.js-stockist-option').addClass('active');
-        $('li.stockist_items').addClass('active');
-    } else {
-        $('li.stockist_items')
-            .filter('[data-tag="' + hash + '"]')
-            .addClass('active');
-    }
-});
-
-$('.js-stockist-option').click(function () {
-    filterCategory($(this).attr('data-filter-tag'));
-    $(this).addClass('active');
-});
