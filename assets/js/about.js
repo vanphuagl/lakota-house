e40482f6b69490fc28936b05b75580841c661e72
unknown <nguyen_van_phu@allgrow-labo.jp>
2024-06-14 08:40:58 +0700

    update about.js

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
         sectionSelector: ".vertical-scrolling",
-        anchors: ["1", "2", "3", "4", "5", "6"],
+        anchors: ["1", "2", "3", "4", "5"],
         controlArrows: false,
         scrollingSpeed: 1200,
         easingcss3: "cubic-bezier(.5,.03,0,.99)",
@@ -11,12 +11,6 @@ $(document).ready(function () {
         autoScrolling: true,
         afterLoad: function (anchorLink, index) {
             scrolling = false;
-            if (index === 6) {
-                $('.c-header').removeClass("c-header--white");
-            } else {
-                $('.c-header').addClass("c-header--white");
-
-            }
         },
         onLeave: function (index, nextIndex, direction) {
             scrolling = true;
@@ -41,12 +35,31 @@ $(document).ready(function () {
         $normalScroll.scroll(function () {
             top = $(this).scrollTop(), bottom = top + window_height2
         });
-        $normalScroll.bind("mousewheel", function (e) {
-            e.originalEvent.wheelDelta / 120 > 0
-                ? $normalScroll >= window_height2
-                    ? top == 0 && $.fn.fullpage.moveSectionUp()
-                    : scrolling || $.fn.fullpage.moveSectionUp()
-                : bottom == end && $.fn.fullpage.moveSectionDown()
-        });
+        // $normalScroll.bind("mousewheel", function (e) {
+        //     e.originalEvent.wheelDelta / 120 > 0
+        //         ? $normalScroll >= window_height2
+        //             ? top == 0 && $.fn.fullpage.moveSectionUp()
+        //             : scrolling || $.fn.fullpage.moveSectionUp()
+        //         : bottom == end && $.fn.fullpage.moveSectionDown()
+        // });
+    }
+});
+
+$(".scrollable-element").on("scroll mousewheel touchmove", function () {
+    let top = -1,
+        offset = $(".js-offset-top").offset().top + $(window).height();
+
+    top = $(this).scrollTop();
+
+    if (top > 1) {
+        $.fn.fullpage.moveSectionDown();
+    } else if (top == 0) {
+        $.fn.fullpage.moveTo(4);
+    }
+
+    if (top > offset) {
+        $(".c-header").removeClass("c-header--white");
+    } else {
+        $(".c-header").addClass("c-header--white");
     }
 });
